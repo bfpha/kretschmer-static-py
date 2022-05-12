b94f0176b8a86ff78f96e04ef82cfadbc18bcf1a
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-05-12 13:09:15 +0200

    minro changes

---
+++ html/static/js/osd_single.js
@@ -0,0 +1,46 @@
+function load_image(el) {
+    var $this = $(el);
+    var image_id = $this.attr("id");
+    console.log(image_id);
+    console.log(el);
+    if ( $this.children("div[class='openseadragon-container']").length == 0 ) {
+        // console.log(osd_container_id);
+        $this.css({
+            'height': '600px'
+        });
+        // OpenSeaDragon Image Viewer
+        var image = $this.attr("data-target");
+        var imageURL = {type: 'image', url: image};
+        var viewer = OpenSeadragon({
+            id: image_id,
+            prefixUrl: 'https://cdnjs.cloudflare.com/ajax/libs/openseadragon/2.4.1/images/',
+            // sequenceMode: true,
+            // showReferenceStrip: true,
+            // showNavigator: true,
+            // imageLoaderLimit: 10,
+            tileSources: imageURL
+        });
+
+        // hide loading spinner if image fully loaded status changes
+        // see issue: https://github.com/openseadragon/openseadragon/issues/1262
+
+        viewer.addHandler('open', function() {
+            var tiledImage = viewer.world.getItemAt(0);
+            if (tiledImage.getFullyLoaded()) {
+                hideLoading();
+            } else {
+                tiledImage.addOnceHandler('fully-loaded-change', hideLoading);
+            }
+        });
+    }
+
+    function hideLoading() {
+        // var container = $(osd_container_id).attr("id");  
+        var spinnerID = "spinner_" + osd_container_id;
+        if ( $("#" + spinnerID ) ) {
+            // console.log(spinnerID);
+            $("#" + spinnerID ).remove();
+        }
+    }
+}
+console.log($("#img-back").attr("id"));
